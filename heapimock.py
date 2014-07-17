6726ade31b5c99fd72a776cdb9c63956a2a0805f
KCU <kamil.chudy@blstream.com>
2014-07-17 12:09:37 +0200

    Added local storage and https support.

---
+++ heapimock.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import cross_origin
+import sys
+
+app = Flask(__name__)
+
+
+@app.route("/users/resolve/")
+@cross_origin()
+def resolve_user():
+    return jsonify({
+        'resolved': True,
+        'stillRunning': False,
+        'source': 'header',
+        'token': '000000000000',
+        'expires': '2014-08-01T04:00:33+00:00',
+        'tetheringConflict': False,
+        'secure': False,
+        'validated': False,
+        'MSISDN': '0048000000000',
+        'ACR': '',
+    })
+
+
+@app.route("/http.html")
+def http():
+    return render_template("/http.html")
+
+
+@app.route("/https.html")
+def https():
+    return render_template("/https.html")
+
+
+@app.route("/intermediatehttp.html")
+def intermediatehttp():
+    return render_template("/intermediatehttp.html")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == 'secure':
+        app.run('0.0.0.0', debug=True, port=5100, ssl_context='adhoc')
+    else:
+        app.run('0.0.0.0', debug=True, port=5000)
---
+++ static/he.js
@@ -0,0 +1,68 @@
+var HE = {
+    options: {
+        resolveUserUrl: null,
+        httpHostedPage: null,
+        redirectUrl: null
+    },
+
+    init: function (options) {
+        for (var key in options) {
+            this.options[key] = options[key];
+        }
+
+        this.storage = new Persist.Store('he');
+
+        return this;
+    },
+
+    getUserDetail: function (callback) {
+        if (this.storage.get('userDetail')) {
+            console.log('Retrieving data from local storage');
+
+            callback(JSON.parse(this.storage.get('userDetail')));
+        } else if (window.location.hash) {
+            console.log('Retriving data from anchor');
+
+            var data = JSON.parse(window.location.hash.substring(1));
+
+            window.location.hash = '';
+
+            this.storage.set('userDetail', JSON.stringify(data));
+
+            callback(data);
+        } else {
+            console.log('Could not find data neither in local storage nor in anchor');
+
+            var protocol = window.location.protocol;
+
+            console.log('Protocol is ' + protocol);
+
+            if (protocol === 'https:') {
+                this._redirectToHttp();
+            } else {
+                console.log('Getting user details from ' + this.options.resolveUserUrl);
+
+                var _this = this;
+
+                $.ajax({
+                    type: 'GET',
+                    async: false,
+                    url: this.options.resolveUserUrl,
+                    success: function (data, textStatus, request) {
+                        _this.storage.set('userDetail', JSON.stringify(data));
+
+                        if (callback) {
+                            callback(data);
+                        }
+                    }
+                });
+            }
+        }
+    },
+
+    _redirectToHttp: function() {
+        console.log('Redirecting to http page at ' + this.options.httpHostedPage);
+
+        window.location = this.options.httpHostedPage + '?redirectUrl=' + this.options.redirectUrl;
+    }
+}
