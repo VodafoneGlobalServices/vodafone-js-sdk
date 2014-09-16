b511deb1ccb5e2dcd880ab8fe9d8f5ced01b9a70
Kamil Chudy <kam.chudy@gmail.com>
2014-09-16 16:22:10 +0200

    Added check status to mock server.

--- heapimock.py
+++ heapimock.py
@@ -1,18 +1,21 @@
-from flask import Flask, jsonify, render_template
+from random import randint
+from flask import Flask, jsonify, render_template, Response
 from flask_cors import cross_origin
 import sys
 
 app = Flask(__name__)
 
 
-@app.route("/users/resolve/", methods=['POST'])
+@app.route("/users/tokens/", methods=['POST'])
 @cross_origin()
 def resolve_user():
-    return jsonify({
-        "token": "b949a8e091294fe38525ed0b1561d191",
-        "acr": "204004STATRSV2014-09-10T13:59:29ZjiDcarz5BLCUrIR875pza14gRqP8wOlKfKMA1gfduV2vZXUSJzt4Kc+Q0qUAPO9XC5aLhbxmO00ZR4WVxi1ZMDvP/Wooo/FoA0kedBBIAdeC/yylwlILVlorijDaP/P3R3TzE6zmUDM7zuXbuLwjM+/H7SvTr5pSBopP6rW4tneXZEgBarJ4imCGlf4Q2eOtqkXHJJxn4UmDWNFRo7O+tF52jAnE1mEIEXsdld/M5guMbP6IJs77HECU7jYbYOu+ycty+ovkni+k8L3NdWAxHCkE4CgpC3qKYt6QJkdkrMcO3TsHD+ODcKTclIvtctWcOtcJM4nsV47msWJN6pkZfg==",
-        "expiresIn": 298741
-    })
+    return _random_response()
+
+
+@app.route("/users/tokens/<token>", methods=['GET'])
+@cross_origin()
+def check_status(token=None):
+    return _random_response(token)
 
 
 @app.route("/oauth/access-token/", methods=['POST'])
@@ -38,6 +41,28 @@ def https():
 @app.route("/intermediatehttp.html")
 def intermediatehttp():
     return render_template("/intermediatehttp.html")
+
+
+def _user_resolved(token):
+    return jsonify({
+        "token": token,
+        "acr": "204004STATRSV2014-09-10T13:59:29ZjiDcarz5BLCUrIR875pza14gRqP8wOlKfKMA1gfduV2vZXUSJzt4Kc+Q0qUAPO9XC5aLhbxmO00ZR4WVxi1ZMDvP/Wooo/FoA0kedBBIAdeC/yylwlILVlorijDaP/P3R3TzE6zmUDM7zuXbuLwjM+/H7SvTr5pSBopP6rW4tneXZEgBarJ4imCGlf4Q2eOtqkXHJJxn4UmDWNFRo7O+tF52jAnE1mEIEXsdld/M5guMbP6IJs77HECU7jYbYOu+ycty+ovkni+k8L3NdWAxHCkE4CgpC3qKYt6QJkdkrMcO3TsHD+ODcKTclIvtctWcOtcJM4nsV47msWJN6pkZfg==",
+        "expiresIn": 298741
+    })
+
+
+def _user_still_resolving(token):
+    response = Response(status=302)
+    response.headers['Location'] = '/users/tokens/%s' % token
+    response.headers['Retry-After'] = '10000'
+    return response
+
+
+def _random_response(token='b949a8e091294fe38525ed0b1561d191'):
+    if randint(1, 4) % 4 == 0:
+        return _user_resolved(token)
+    else:
+        return _user_still_resolving(token)
 
 
 if __name__ == "__main__":
