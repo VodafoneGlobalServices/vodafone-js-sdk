af4b5dd23004c8f2f9083acc4e08b095e185c0c2
Kamil Chudy <kam.chudy@gmail.com>
2014-09-25 15:01:43 +0200

    Added grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: 'src/he.js',
+                dest: 'dist/he.min.js'
+            },
+        },
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
